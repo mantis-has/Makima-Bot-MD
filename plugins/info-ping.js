6de838b4df6ae17b86ed140082fcc8621493e3b0
Ado <me.ado926codes@gmail.com>
2025-06-26 13:09:53 -0600

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          conn.reply(m.chat, `☄︎ *Velocidad Actualmente*\n> Tiempo : ⴵ ${latensi.toFixed(4)}ms`, m, rcanal);
+          conn.reply(m.chat, `☄︎ *Velocidad Actualmente*\n> Tiempo : ⴵ ${latensi.toFixed(4)}mss`, m, rcanal);
             });
 }
 handler.help = ['ping']
