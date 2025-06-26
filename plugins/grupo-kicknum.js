8fc0d948534055d7fc540fa7c8f8cce01b463dac
Ado <me.ado926codes@gmail.com>
2025-06-26 15:23:42 -0600

    Actualizar grupo-kicknum.js

--- plugins/grupo-kicknum.js
+++ plugins/grupo-kicknum.js
@@ -2,7 +2,7 @@ import { igdl } from 'ruhend-scraper';
 
 const handler = async (m, { args, conn }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, `${emoji} Por favor, ingresa un enlace de Instagram.`, m);
+    return conn.reply(m.chat, `Por favor, ingresa un enlace de Instagram.`, m);
   }
 
   try {
@@ -11,11 +11,11 @@ const handler = async (m, { args, conn }) => {
     const data = res.data;
 
     for (let media of data) {
-      await conn.sendFile(m.chat, media.url, 'instagram.mp4', `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`, m);
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', `*Aqui tienes ฅ^•ﻌ•^ฅ.*`, m);
     await m.react(done);
     }
   } catch (e) {
-    return conn.reply(m.chat, `${msm} Ocurrió un error.`, m);
+    return conn.reply(m.chat, ` Ocurrió un error.`, m);
     await m.react(error);
   }
 };
